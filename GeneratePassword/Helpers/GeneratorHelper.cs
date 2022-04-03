105e60dc69d083d1a010ca194c0491c3de26fb20
Carol Omar <94629010+cazie@users.noreply.github.com>
2022-04-03 02:03:03 -0400

    refactoring
    making more complex with separate randoms

--- GeneratePassword/Helpers/GeneratorHelper.cs
+++ GeneratePassword/Helpers/GeneratorHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GeneratePassword.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,35 +21,71 @@ namespace GeneratePassword.Helpers
 
 
             Random rand = new Random();
+            Random randCaps = new Random();
+            Random randSymbols = new Random();
+            Random randNumbers = new Random();
 
             string output = "";
+            int minCaps = 2;
+            int minSymbols = 2;
+            int minNumbers = 2;
             if (Caps == true)
             {
-                selectCharacters += selectCharacters.ToUpper();
+                selectCharacters = selectCharacters.ToUpper();
+
+                for (int i = 0; i < minCaps; i++)
+                {
+                int numCaps = randCaps.Next(minCaps);
+                output += numCaps;
+                }
+             
 
             }
 
             if (Symb == false)
             {
                 symbs = "";
+                
 
+            }
+            else
+            {
+               
+                    selectCharacters += symbs;
+
+                for (int i = 0; i < minSymbols; i++)
+                {
+                    int numSymbols = randSymbols.Next(minSymbols);
+                    output += numSymbols;
+                }
             }
 
             if (numbs == false)
             {
                 numbers = "";
             }
-            selectChoice = selectCharacters + symbs + numbers;
+            else
+            {
+                selectCharacters += numbers;
+                for (int i = 0; i < minNumbers; i++)
+                {
+                    int numNumbers = randNumbers.Next(minNumbers);
+                    output += numNumbers;
+                }
+            }
+            selectChoice = selectCharacters + output;
 
-            for (int i = 0; i < maxLen; i++)
+            for (int i = 0; i < maxLen - (output.Length); i++)
             {
 
-                int num = rand.Next(selectChoice.Length);
+                int num = rand.Next(selectCharacters.Length);
 
                 output += selectChoice[num];
 
             }
             return output;
         }
+
+     
     }
 }
---
+++ GeneratePassword/Models/AccountModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GeneratePassword.Models
+{
+    public class AccountModel
+    {
+        public string Account { get; set; }
+
+        public string Description { get; set; }
+
+        public string Password { get; set; }
+    }
+}
